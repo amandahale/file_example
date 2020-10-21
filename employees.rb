8f4db197b1a725080ba86c7448d0759ad1e49555
Amanda <amanda@actualize.co>
2020-10-21 13:31:44 -0500

    reorganize data to put manager class in it's own file

--- employees.rb
+++ employees.rb
@@ -45,17 +45,15 @@ class Employee
 
 end
 
-class Manager < Employee
-  def send_report
-    puts "Sending email..."
-    #use an email sending library
-    puts "Email sent!"
-  end
+employee1 = Employee.new({first_name: "Majora", last_name: "Carter", salary: 80000, active: true})
+employee2 = Employee.new(last_name: "Campos", first_name: "Danilo", salary: 70000, active: true)
+employee1.print_info
+employee2.print_info
+employee2.give_annual_raise
+employee2.print_info
+puts employee1.first_name
+puts employee1.last_name
+puts employee1.active
+employee2.active=(false)
+puts employee2.active
 
-end
-
-
-
-manager = Manager.new(first_name: "Saron", last_name: "Yitbarek", salary: 100000, active: true)
-manager.print_info
-manager.send_report
